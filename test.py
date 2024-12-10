600d916e89c59e71cb2a4fbec667ab98c814d79f
kimsy <kimsy@codecraft.co.kr>
2024-12-10 17:31:31 +0900

    key hide

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from langchain.chat_models import ChatOpenAI, ChatAnthropic
 
 llm = OpenAI(
   model_name="davinci-002",
-  openai_api_key="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"
+  # openai_api_key=""
 )
 
 chat = ChatOpenAI()
